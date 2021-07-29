18cad03e1752c800059fb7fe3f6eed1feca4a63a
Undefined <contact.pritulx@gmail.com>
2021-07-29 15:58:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+
+def time():
+    string = strftime('%I:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+label = Label(root, font=("Retroica", 80),
+              background="#1f1f1f", foreground="white")
+label.pack(anchor="center")
+
+time()
+
+mainloop()
