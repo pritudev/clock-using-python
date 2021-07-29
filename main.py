be5d68763f817b0cefc3c7408eb506e47b8b6b38
Undefined <contact.pritulx@gmail.com>
2021-07-29 20:31:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from tkinter.ttk import *
 from time import strftime
 
 root = Tk()
-root.title("Clock")
+root.title("Clock Using Python")
+p1 = PhotoImage(file='G:\IMAGES\Others\pngaaa.com-3626848.png')
+root.iconphoto(False, p1)
 
 
 def time():
@@ -12,9 +14,9 @@ def time():
     label.after(1000, time)
 
 
-label = Label(root, font=("Retroica", 80),
+label = Label(root, font=("Roboto Black", 100),
               background="#1f1f1f", foreground="white")
-label.pack(anchor="center")
 
+label.pack(anchor="center")
 time()
 mainloop()
